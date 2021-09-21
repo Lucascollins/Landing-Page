9314c0a620bde466eb71282d24d77fc4f8cafc60
Lucascollins <lucascollins506@gmail.com>
2021-09-21 20:41:47 -0300

    Changed color

--- landing page/script.js
+++ landing page/script.js
@@ -6,16 +6,33 @@ document.getElementById("forms")
 function verificar() {
     const email = document.getElementById("email")
     const nome = document.getElementById("name")
+    var obj = {
+        email:email.value,
+        nome:nome.value
+    }
 
     if (email.value && nome.value) {
-        alert("Todo certo " + email.value + " " + nome.value)
+        ConfirmLogin()
+        console.log(obj)
     } else {
-        alert("Preencha seus Dados")
+       NotConfirmLogin()
     }
 
     email.value = ""
     nome.value = ""
     
 }
+function ConfirmLogin(){
+    const login = document.getElementById("login")
+    const span = document.getElementById("span")
+    login.style.display = "block"
+    span.textContent = "Enviado com Sucesso"
 
+}
+function NotConfirmLogin(){
+    const login = document.getElementById("login")
+    const span = document.getElementById("span")
+    login.style.display ="block"
+    span.textContent = "Errou"  
+}
 
