c72fcc1616cf9ba63b6681d422c6f1ed876e3c9c
Lucascollins <lucascollins506@gmail.com>
2021-09-22 20:44:46 -0300

    Adicionando animações em forms

--- landing page/script.js
+++ landing page/script.js
@@ -3,6 +3,12 @@ document.getElementById("forms")
         event.preventDefault()
     })
 
+
+const login = document.getElementById("login")
+const span = document.getElementById("span")
+const send = document.getElementById("underline-send")
+
+
 function verificar() {
     const email = document.getElementById("email")
     const nome = document.getElementById("name")
@@ -11,28 +17,45 @@ function verificar() {
         nome:nome.value
     }
 
-    if (email.value && nome.value) {
+    //Regex
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    //Confirmation email
+    var ver  = email.value.includes("@")
+    var verificar =  emailRegex.test(email.value)
+
+    if (nome.value && verificar) {
         ConfirmLogin()
         console.log(obj)
     } else {
        NotConfirmLogin()
     }
 
+
+    setTimeout(()=>noneModel(login),2000)
+
     email.value = ""
     nome.value = ""
     
 }
 function ConfirmLogin(){
-    const login = document.getElementById("login")
-    const span = document.getElementById("span")
-    login.style.display = "block"
+    login.style.display = "flex"
     span.textContent = "Enviado com Sucesso"
+    send.style.backgroundColor = "green"
 
 }
 function NotConfirmLogin(){
-    const login = document.getElementById("login")
-    const span = document.getElementById("span")
-    login.style.display ="block"
+    login.style.display ="flex"
     span.textContent = "Errou"  
+    send.style.backgroundColor = "red"
 }
 
+function noneModel(element){
+    
+    element.style.display = "none"
+}
+
+//arrowfunction,o porque usar ela
+const displayNone = (element) => { 
+    element.style.display = "none"
+}
